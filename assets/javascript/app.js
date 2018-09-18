45add5b27d0923e98671206c512b461a4a95ea84
cholo5ntrol <raglaks94@gmail.com>
2018-09-17 22:03:10 -0500

    added pics for project; layed out basic structure of app

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDdbCzSttyyrPSblcRWczN1ph-PL9wJSH4",
+    authDomain: "rps-multiplayer-33a99.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-33a99.firebaseio.com",
+    projectId: "rps-multiplayer-33a99",
+    storageBucket: "rps-multiplayer-33a99.appspot.com",
+    messagingSenderId: "362020719621"
+};
+firebase.initializeApp(config);
+
+
