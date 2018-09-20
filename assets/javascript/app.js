c890657a896e5daa068e78aeeb1e2c89bce90915
cholo5ntrol <raglaks94@gmail.com>
2018-09-19 22:37:37 -0500

    redesigned app to use firebase more effectively; finished start page and currently working on player vs pc game mode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,4 +8,67 @@ var config = {
 };
 firebase.initializeApp(config);
 
+//var database = firebase.database();
+
+var refObj = firebase.database().ref();
+
+$(document).ready(function () {
+
+    refObj.on("value", function (snapshot) {
+        console.log(snapshot.val());
+    });
+
+    $("#pvpc").on("click", function () {
+        playerName();
+    });
+
+    function playerName() {
+        $("#sub").text(" ");
+        $("#content").text(" ");
+        var form = $("<form>");
+        var input = $("<input>");
+        var button = $("<button>");
+        var h2 = $("<h2>");
+
+        input.attr("type", "text");
+        input.attr("class", "form-control text-center");
+        input.attr("id", "name");
+
+        button.attr("type", "button");
+        button.attr("class", "btn btn-danger text-center");
+        button.attr("id", "submit");
+        button.text("Submit");
+
+        form.append(input);
+        form.append(button);
+
+        h2.text("What is your name?");
+
+        $("#sub").append(h2);
+        $("#content").append(form);
+
+        nameToFire();
+
+    }
+
+    function nameToFire() {
+        $("#submit").on("click", function () {
+            var submit = $("#name").val();
+
+
+            refObj.set({
+                player1: submit,
+            });
+
+            $("#sub").text(" ");
+            $("#content").text(" ");
+
+            gamePage();
+        });
+    }
+
+    function gamePage() {
+        var h2
+    }
+});
 
